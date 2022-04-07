ba5b43ae67c12192ad6770841fa50733ecea63c8
bo-wambui <bowambui08@gmail.com>
2022-04-07 03:44:13 +0000

    Static library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ *
+ *
+ *
+ *
+ *
+*/
+int addnumbers(int a, int b);
+
+int main(void)
+{
+	int total;
+	total = addnumbers(5, 7);
+
+	printf("total = %d\n", total);
+	return (0);
+
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+static_libraries project tasks
