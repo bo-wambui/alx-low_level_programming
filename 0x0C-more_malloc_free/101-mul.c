40c0b805220e12360f1e6012ce2d96341cb13be2
bo-wambui <bowambui08@gmail.com>
2022-04-08 01:18:54 +0000

    memory allocation

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,9 +12,9 @@ int is_digit(char *s)
 {
 	int i = 0;
 
-	while (num[i])
+	while (s[i])
 	{
-		if (num[i] < '0' || num[i] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
 		i++;
 	}
@@ -27,11 +27,11 @@ int is_digit(char *s)
  *
  * Return: the length of the string
  */
-int _strlen(char *num)
+int _strlen(char *s)
 {
 	int i = 0;
 
-	while (num[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
@@ -56,14 +56,14 @@ void errors(void)
  */
 int main(int argc, char *argv[])
 {
-	char *num1, *num2;
+	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	num1 = argv[1], num2 = argv[2];
-	if (argc != 3 || !is_digit(num1) || !is_digit(num2))
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-	len1 = _strlen(num1);
-	len2 = _strlen(num2);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
@@ -72,11 +72,11 @@ int main(int argc, char *argv[])
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = num1[len1] - '0';
+		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = _strlen(num2) - 1; len2 >= 0; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			digit2 = num2[len2] - '0';
+			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
